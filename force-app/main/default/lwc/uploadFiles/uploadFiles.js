e27ad8e759a48858333b27f1744821645d3889bc
Luca Bobbio <bobbio.luca.117@gmail.com>
2024-02-20 12:17:39 -0300

    Add JSDoc for documentation of code

--- force-app/main/default/lwc/uploadFiles/uploadFiles.js
+++ force-app/main/default/lwc/uploadFiles/uploadFiles.js
@@ -5,6 +5,12 @@ import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import createDocRecord from '@salesforce/apex/UploadFilesController.createDocRecord'
 
+const ONE_SECOND_DELAY = 1000;
+
+/**
+ * Represents the UploadFiles component responsible for uploading files.
+ * @extends {NavigationMixin(LightningElement)}
+ */
 export default class UploadFiles extends NavigationMixin(LightningElement) {
     @track urlValue;
     @track title = '';
@@ -13,6 +19,10 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
     @track previewImage = false;
     @track fieldData = [];
 
+    /**
+     * Map containing URL prefixes for different file types.
+     * @type {Object}
+     */
     @track urlMap = {
         '.pdf': 'data:application/pdf;base64,',
         '.png': 'data:image/png;base64,',
@@ -25,6 +35,10 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
         '.docx': 'data:application/vnd.openxmlformats-officedocument.wordprocessingml.document;base64,'
     };
 
+    /**
+    * Acceptable file extensions for upload file
+    * @returns {Array}  An array of strings of acceptable file extensions
+    */
     get acceptedFormats() {
         return [
             '.pdf', 
@@ -39,10 +53,18 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
         ];
     }
 
+    /**
+    * Checks if any file in the fieldData has a image extension (png,jpg,jpeg)
+    * @returns {boolean} True if at least one file has a image extension, false otherwise 
+    */
     get getPreviewImage(){
         return this.fieldData.some(file => /\.(jpg|jpeg|png)$/i.test(file.filename));
     }
 
+    /**
+    * Opens the file dialog and processes the selected file
+    * @param {Event} event - The file upload event containing the selected file 
+    */
     openfileUpload(event){
         const file = event.detail.files[0]
         const acceptedFormats = this.acceptedFormats;
@@ -69,23 +91,42 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
         }
     }
 
+    /**
+    * Handle changes from the lightning-input field and assings to title
+    * @param {Event} event - The lightning-input event containing the new title value
+    */
     handleTitle(event){
         this.title = event.target.value
     }
 
+    /**
+    * Handle changes from the lightning-textarea field and assings to description
+    * @param {Event} event - The lightning-textarea input event containing the new description value
+    */
     handleDescription(event){
         this.description = event.target.value
     }
 
+    /**
+    * Sets the previewImage flag to true and updates the urlValue based on clicked element's dataset id 
+    * @param {Event} event - The click event triggering the file preview
+    */
     handlePreviewFile(event){
         this.previewImage = true;
         this.urlValue = event.currentTarget.dataset.id
     }
 
+    /**
+    * Sets the previewImage flag to false, hiding the file preview and closing pop-up
+    */
     handleCancel(){
         this.previewImage = false;
     }
 
+    /**
+    * Removes the file associated with the clicked element's dataset id from the fieldData array.
+    * @param {Event} event - The click event triggering the file deletion
+    */
     handleDeleteFile(event){
         const itemToRemove = this.fieldData.find(
             element => element.url == event.currentTarget.dataset.id
@@ -95,12 +136,21 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
         })
     }
 
+    /**
+    * Handles the save file action by invoking the onSaveFile method after a short delay
+    */
     handleSaveFile(){
         setTimeout(() => {
             this.onSaveFile();
-        }, 100);
+        }, ONE_SECOND_DELAY);
     }
 
+    /**
+    * Handles the file saving process by initiating the creation of a new document record with.
+    * If the description is not empty and at least one file is selected, it calls the createDocRecord Apex method.
+    * If successful, it navigates to the newly created document record page. If unsuccessful, it logs the error 
+    * message and displays an error toast.
+    */
     onSaveFile(){
         this.showSpinner = true;
         if(this.description && !(this.fieldData.length == 0)){
@@ -124,8 +174,7 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
                         'Sucesso', 
                         'Arquivo criado com sucesso!'
                     )
-                    this.navigateToDocumentRecordPage(recordId)
-                    //window.location.reload()
+                    this.navigateToDocumentRecordPage(recordId);
                 }
             }).catch((error) =>{
                 console.error(error);
@@ -146,6 +195,10 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
         }
     }
 
+    /**
+    * Navigates to the record page of a Document__c record with the specified recordId
+    * @param {string} recordId - The Id of the Document record to navigate to.
+    */
     navigateToDocumentRecordPage(recordId) {
         this[NavigationMixin.Navigate]({
             type: 'standard__recordPage',
@@ -156,7 +209,13 @@ export default class UploadFiles extends NavigationMixin(LightningElement) {
             }
         });
     }
-    
+
+    /**
+    * Displays a toast message with the specified type, title, and message.
+    * @param {string} type - The type of toast message (error, warning, success).
+    * @param {string} title - The title of the toast message.
+    * @param {string} message - The content of the toast message.
+    */
     showToast(type, title, message) {
         let event = new ShowToastEvent({
             variant: type,
